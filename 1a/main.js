01106761cfa63b3583e30dd45245bc81d0868bf6
WiLdPUG <kt222td@student.lnu.se>
2026-01-20 15:52:27 +0100

    Lab 1-a: Initial commit with math functions

---
+++ 1a/main.js
@@ -0,0 +1,14 @@
+import divide, { add as addition , subtract , multiply ,SECRET_SAUCE } from './math.js';
+import { PI as piValue } from './constants.js';
+import * as MathUtils from './math.js';
+
+
+
+console.log(subtract(4, 1));
+console.log(multiply (5,10));
+console.log(divide(10, 2));
+console.log(SECRET_SAUCE);
+console.log(piValue);
+console.log(addition(5, 20));
+console.log(MathUtils.add(2, 3));
+console.log(MathUtils.multiply(4, 5));
