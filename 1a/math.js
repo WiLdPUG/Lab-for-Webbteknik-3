5cba0abb887efc3d2676530306d0a4f595462ca8
Kevin <kt222td@student.lnu.se>
2026-01-21 14:43:47 +0100

    pushed

--- 1a/math.js
+++ 1a/math.js
@@ -20,3 +20,9 @@ export function circleArea(radius) {
 
 export const PI = 3.14159;
 export const SECRET_SAUCE = 42;
+
+import { rectangleArea, rectanglePerimeter } from "../rectangles";
+
+
+console.log(`Area of 5x10 rectangle: ${rectangleArea(5, 10)}`);     
+console.log(`Perimeter of 5x10 rectangle: ${rectanglePerimeter(5, 10)}`); 
